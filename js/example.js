0f304fa8bca276e45688c93e23f9bb12640a26cc
Nickmescher <Ferbuscr22>
2022-12-13 16:23:50 +0300

    feat: lab7

--- js/example.js
+++ js/example.js
@@ -1,5 +1,9 @@
 import Swiper from "swiper";
 
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
 const swiper = new Swiper(".swiper", {
     // navigation elements
     navigation: {
